c6f6bda6e1c3cdf14009a0caf1e6797c493c429f
paratellaDev <paratelladevmarcos@gmail.com>
2024-06-19 19:19:10 -0300

    inserindo o formModal

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,25 +1,22 @@
 import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
-export default function NavBar() {
+const NavBar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
-
     const handleMenuItemClick = () => {
         setMenuOpen(false);
     };
-
     const [modal, setModal] = useState(false);
     if (modal) {
         document.body.classList.add('active-modal');
     } else {
         document.body.classList.remove('active-modal');
     }
-
     const handleFormSubmit = () => {
         setModal(false);
     };
     return (
-        <nav>
+        <><nav>
             <input type="button" id='click' checked={menuOpen} onChange={setMenuOpen} />
             <label htmlFor="click" className='manu-btn'>
                 <i className='fa fa-bars'></i>
@@ -37,21 +34,20 @@ export default function NavBar() {
                 <li><NavLink to='/contact' onClick={handleMenuItemClick}>Contato</NavLink></li>
             </ul>
         </nav>
+            <div className=''>
+                <div className=''></div>
+                <h2 className=''>Restaurante Boa Aberta</h2>
+                <p className=''>Restaurante</p>
+                <h2>Assine nossa newsletter</h2>
+                <p>Assinatura Gratuíta, assine e ganhe 20% OFF no primeiro consumo</p>
 
+                <form onSubmit={handleFormSubmit}>
+                    <input className='' type="email" required placeholder='Seu Email'/>
+                    <label htmlFor=""></label>
+                </form>
+            </div>
+        </>
 
-
-
-        // <nav>
-        //     <button onClick={() => setMenuOpen(!menuOpen)}>
-        //         {menuOpen ? 'Fechar Menu' : 'Abrir Menu'}
-        //     </button>
-        //     {menuOpen && (
-        //         <ul>
-        //             <li onClick={handleMenuItemClick}>Início</li>
-        //             <li onClick={handleMenuItemClick}>Contato</li>
-        //             <li onClick={handleMenuItemClick}>Serviços</li>
-        //         </ul>
-        //     )}
-        // </nav>
     );
 }
+export default NavBar;
